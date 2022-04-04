a689f183a56b3bff53be9f8ceca5038563b812aa
Temurito <89263357+Temurito@users.noreply.github.com>
2022-04-05 00:27:23 +0300

    Added fails

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Link from "next/link"
+
+const App = () => {
+    return(
+        <div>
+            <p>Abobus</p>
+            <Link href="link-page">OIKOK</Link>
+        </div>
+    )
+}
+export default App;
---
+++ pages/link-page.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Link from "next/link"
+
+const App = () => {
+    return(
+        <Link href="/">OIKOK</Link>
+    )
+}
+
+export default App;
